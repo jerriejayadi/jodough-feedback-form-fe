9bed3c89a444e94f882a8fa2a0f36b5333159a50
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-08-08 07:13:53 +0800

    FIX next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/apis",
-        destination: process.env.HOST, // Proxy to Backend External
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

c7a5e7ecf48a3b79a0e59bb8284f6ab0af4b636f
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-08-19 12:47:06 +0800

    [ADD] Doughtnut Randomizer

--- src/app/closing-page/page.tsx
+++ src/app/closing-page/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { DONUT_IMAGE_LIST } from "@/utils/lib";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const ClosingPage: React.FC = () => {
+  const [selectedURL, setSelectedURL] = useState<string>(
+    "/images/Jodough_Doughnut/donut-1.png"
+  );
+
+  useEffect(() => {
+    setSelectedURL(DONUT_IMAGE_LIST[Math.floor(Math.random() * 19)]);
+  }, []);
   return (
     <main
       className={`p-10 flex flex-col items-center justify-between min-h-screen`}
@@ -16,7 +25,19 @@ const ClosingPage: React.FC = () => {
       >
         Kamu adalah tipe donat...
       </motion.div>
-      <div></div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 100 }}
+        transition={{ delay: 4, duration: 2 }}
+      >
+        <Image
+          className={`w-96 object-cover`}
+          alt={``}
+          src={selectedURL}
+          width={1080}
+          height={1080}
+        />{" "}
+      </motion.div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 100 }}
